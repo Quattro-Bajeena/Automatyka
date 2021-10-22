cc52a5ef62ec013eab9c3937d6c3efbcf010df27
Quattro-Bajeena <motmen001@gmail.com>
2021-10-22 17:49:45 +0200

    completed PID controller and its website

--- water_tank.py
+++ water_tank.py
@@ -2,7 +2,7 @@ import math, time, csv, json
 from pathlib import Path
 import matplotlib
 
-# matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 PLOT_FOLDER = Path("plots")
@@ -26,11 +26,11 @@ CONTROL_SIGNAL_MAX = 10
 
 
 def simulate_watertank(tank_area, tank_height, outflow_coef, max_inflow_rate,
-                       sampling_period,  simulation_time,
+                       sampling_period, simulation_time,
                        signal_amplification, control_signal_max, target_level, doubling_time, lead_time):
     time_elapsed = 0  # s
     current_height = 0  # m
-    difference = 0  # m
+    difference = target_level - current_height  # m
     control_signal = 0
 
     time_samples = [time_elapsed]
@@ -64,13 +64,45 @@ def simulate_watertank(tank_area, tank_height, outflow_coef, max_inflow_rate,
         level_differences.append(difference)
         signals.append(control_signal)
 
-    result = {"water_heights": water_heights, "time_samples": time_samples, "signals" : signals}
+    final_error = target_level - current_height
+    overshoot = ((max(water_heights) - target_level) / target_level) * 100  # %
+
+    threshold = target_level * 0.05
+    lower_bound = target_level - threshold
+    upper_bound = target_level + threshold
+
+    regulation_time = time_elapsed
+    for i, level in enumerate(reversed(water_heights)):
+        if level >= upper_bound or level <= lower_bound:
+            regulation_time = time_elapsed - (i * sampling_period)
+            break
+
+    integral_regulation_accuracy_indicator = sampling_period * sum([abs(x) for x in level_differences])
+    integral_regulation_accuracy_indicator_2 = sampling_period * sum([x * x for x in level_differences])
+
+    integral_regulatory_cost_indicator = sampling_period * sum([abs(x) for x in signals])
+    integral_regulatory_cost_indicator_2 = sampling_period * sum([x * x for x in signals])
+
+    result = {
+        "target_level" : target_level,
+        "water_heights": water_heights,
+        "time_samples": time_samples,
+        "signals": signals,
+        "quality_indicators": {
+            "final_error": round(final_error, 5),
+            "overshoot": round(overshoot, 1),
+            "regulation_time": regulation_time,
+            "Ie": round(integral_regulation_accuracy_indicator, 1),
+            "Ie2": round(integral_regulation_accuracy_indicator_2, 1),
+            "Iu": round(integral_regulatory_cost_indicator, 1),
+            "Iu2": round(integral_regulatory_cost_indicator_2, 1)
+        }}
+
     return result
 
 
 def plot_save_water_levels(data, save_plot, plot_folder, data_folder):
     fig, ax1 = plt.subplots()
-
 
     color = 'tab:blue'
     ax1.set_ylabel("Water level [m]", color=color)
@@ -85,6 +117,9 @@ def plot_save_water_levels(data, save_plot, plot_folder, data_folder):
     ax2.set_ylabel("Signal strength", color=color)
     ax2.plot(data["time_samples"], data["signals"], color=color)
     ax2.tick_params(axis='y', labelcolor=color)
+
+    ax1.axhline(y=data["target_level"], color='y', linestyle='--')
+
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
--- wsgi.py
+++ wsgi.py
@@ -3,17 +3,31 @@ from pathlib import Path
 
 import water_tank
 
-
 app = Flask(__name__)
 
 PLOT_FOLDER = Path("plots")
 DATA_FOLDER = Path("data")
 
+TANK_AREA = 2  # m2
+TANK_HEIGHT = 10  # m
+OUTFLOW_COEF = 0.035  # m5/2
+INFLOW_RATE = 0.05  # m3/s
+SAMPLING_PERIOD = 1  # s
+
+SIMULATION_TIME = 30 * 60  # s
+
+TARGET_LEVEL = 1.5  # m
+
+SIGNAL_AMPLIFICATION = 0.015
+DOUBLING_TIME = 0.5  # s 0.5 to 2
+LEAD_TIME = 0.25  # s
+
+CONTROL_SIGNAL_MAX = 10
+
 
 @app.route("/")
 def index():
-    plot_name = request.args.get('plot_name')
-    return render_template("index.html", plot_filename=plot_name)
+    return render_template("index.html")
 
 
 @app.route("/plots/<filename>")
@@ -26,23 +40,25 @@ def show_water_tank_data(filename):
     return send_from_directory(DATA_FOLDER, filename)
 
 
-@app.route("/water-tank", methods=['POST'])
+@app.route("/water-tank")
 def water_tank_simulation():
-    print(request.form)
-    tank_area = float(request.form['tank-area'])
-    tank_height = float(request.form['tank-height'])
-    outflow_coef = float(request.form['outflow-coef'])
-    inflow_rate = float(request.form['inflow-rate'])
-    sampling_period = float(request.form['sampling-period'])
-    simulation_time = int(request.form['simulation-time'])
-    keep_result = bool(request.form.get('keep-result'))
-    print(keep_result)
+    target_level = float(request.args.get('target-level'))
+    simulation_time = int(request.args.get('simulation-time'))
+    #target_level = float(request.form['target-level'])
+    #simulation_time = int(request.form['simulation-time'])
 
-    result = water_tank.simulate_watertank(tank_area, tank_height, outflow_coef, inflow_rate, sampling_period,
-                                           simulation_time)
-    plot_name = water_tank.plot_save_water_levels(result, keep_result, PLOT_FOLDER, DATA_FOLDER)
+    result = water_tank.simulate_watertank(TANK_AREA, TANK_HEIGHT, OUTFLOW_COEF, INFLOW_RATE,
+                                           SAMPLING_PERIOD, simulation_time,
+                                           SIGNAL_AMPLIFICATION, CONTROL_SIGNAL_MAX, target_level, DOUBLING_TIME,
+                                           LEAD_TIME)
 
-    return redirect(url_for('index', plot_name=plot_name))
+    plot_name = water_tank.plot_save_water_levels(result, True, PLOT_FOLDER, DATA_FOLDER)
+
+    return render_template('index.html',
+                           plot_name=plot_name,
+                           quality_indicators=result["quality_indicators"],
+                           target_level=target_level,
+                           simulation_time=simulation_time)
 
 
 if __name__ == '__main__':
